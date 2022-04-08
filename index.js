a9b247b2a6e7e8a372355cddf8447e96e6886829
lucasfilipe <lucasfilipe2323@icloud.com>
2022-04-07 21:21:44 -0300

    criando funções

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-var nome = 'Lucas';
+function entrar(){
+
+    var area = document.getElementById('area'); // pega o elemento id
+    var nome = prompt('Digite seu nome? '); // armazena 
+
+    if (nome == '' || nome == null) {
+        alert('Digite seu nome novamente');
+        area.innerHTML = 'bem vindo.... ';
+    }
+    else {
+        area.innerHTML = ' bem vindo ' + nome;
+    }
+}
+
+
+function entrar2(nome){
+
+    var area = document.getElementById('area2'); // pega o elemento id
+    var texto = prompt('Digite seu sobrenome? '); // armazena 
+
+    area.innerHTML = nome + " " + texto;
+}

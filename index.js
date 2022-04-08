046e088a44b3803d662fb0dc39ed3257966a807e
lucasfilipe <lucasfilipe2323@icloud.com>
2022-04-07 23:29:01 -0300

    comando switch

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-//while = enquanto
+//switch
 
-var x = 0;
+document.write('</br>Escolha seu pedido');
+document.write('</br>1 - Sorvete');
+document.write('</br>2 - Pizza');
+document.write('</br>1 - Agua');
 
-while (x < 10) {
-    document.write('</br> O valor de x eh =' + x);
-    x++;
-}
+function pedir() {
+    x = prompt('O que deseja pedir?');
 
-document.write('</br>');
-
-//for = para
-var i;
-for (i = 10; i >= 0; i--) {
-    document.write('</br> O valor de x eh =' + i);
+    switch(x){
+        case '1':
+            alert("Seu sorvete está sendo preparado");
+            break;
+        
+        case '2':
+            alert("Sua pizza está sendo preparada");
+            break;
     
+        case '3':
+            alert("Sua agua esta vindo");
+            break;
+        
+    }
 }

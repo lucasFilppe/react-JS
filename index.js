4bdd65de605a655701a7276749d4ebbcc558801b
lucasfilipe <lucasfilipe2323@icloud.com>
2022-04-08 09:39:24 -0300

    temporização

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
-//switch
-
-document.write('</br>Escolha seu pedido');
-document.write('</br>1 - Sorvete');
-document.write('</br>2 - Pizza');
-document.write('</br>1 - Agua');
-
-function pedir() {
-    x = prompt('O que deseja pedir?');
-
-    switch(x){
-        case '1':
-            alert("Seu sorvete está sendo preparado");
-            break;
-        
-        case '2':
-            alert("Sua pizza está sendo preparada");
-            break;
-    
-        case '3':
-            alert("Sua agua esta vindo");
-            break;
-        
-    }
+function acao(){
+    document.write("Executando.... </br>");
 }
+
+acao()
+
+//executa de tempo em tempo
+var timer = setInterval(acao, 1000);
+
+//setTimeout(acao, 3000);

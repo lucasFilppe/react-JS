9e04a4daa1de9659c7914eb06f03316b94e4a6a3
lucasfilipe <lucasfilipe2323@icloud.com>
2022-04-12 11:37:30 -0300

    aplicando class component

--- primeirosite/src/App.js
+++ primeirosite/src/App.js
@@ -1,38 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Equipe = (props) => {
-  return(
-    <div>
-      <Sobre nome={props.nome} cargo={props.cargo} idade={props.idade}/> 
-      <Social fb={props.facebook}/>
-      <hr></hr>
-    </div>
-  )
-}
-
-const Sobre = (props) => {
-  return(
-    <div>
-      <h1>Ola sou {props.nome}</h1>
-      <h2>Cargo: {props.cargo}</h2>
-      <h3>Idade: {props.idade}</h3>
-    </div>
-  )
-}
-
-const Social =(props) => {
-  return(
-    <div>
-      <a href={props.fb}>Facebook</a>
-    </div>
-  )
+class Equipe extends Component{
+  render(){
+    return(
+      
+      //this referencia a este componente
+      <div>
+        <h1>{this.props.nome}</h1>
+        <h2>{this.props.cargo}</h2>
+        <h3>{this.props.idade}</h3>
+      </div>
+      
+    );
+  }
 }
 
 function App() {
   return (
     <div>
-      <Equipe nome="Lucas" cargo="Programador" idade="23" facebook="https://www.facebook.com/lucasfilipe.ramos.10"/>
-      <Equipe nome="Vinicius" cargo="analista de sistemas" idade="25" facebook="https://www.facebook.com/lucasfilipe.ramos.10"/>
+      <Equipe nome="Lucas" cargo="Programador" idade="25"/>
     </div>
   );
 }

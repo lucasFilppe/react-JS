ed55bec1c6ee6947e4c81422bed3d367c49ece3e
lucasfilipe <lucasfilipe2323@icloud.com>
2022-04-15 16:32:11 -0300

    renderização condiocional

--- primeirosite/src/App.js
+++ primeirosite/src/App.js
@@ -1,15 +1,43 @@
 import React, { Component } from 'react';
-import Membro from './Membro';
+
 
 class App extends Component{
 
+  constructor(props){
+    super(props);
+    this.state = {
+      status: true
+    };
+    this.sair = this.sair.bind(this);
+    this.entrar = this.entrar.bind(this);
+  }
+
+  sair(){
+    this.setState({status: false});
+  }
+
+  entrar(){
+      this.setState({status: true});
+    }
 
     render(){
     return(
       
-      <div>
-        <Membro nome="Visitante"/>
-      </div>
+        <div>
+        {this.state.status ?
+          <div>
+            <h1>Bem-vindo ao sitema</h1>
+            <button onClick={this.sair}>Sair</button>
+          </div>:
+
+          <div>
+            <h1>Olá visitante</h1>
+            <button onClick={this.entrar}>Entrar no sistema</button>
+          </div>
+
+        }
+        </div>
+      
       
     );
   }

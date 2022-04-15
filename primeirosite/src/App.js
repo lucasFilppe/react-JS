9d0972b7aee1ee579bc4ffb1fb1243b6a6c17893
lucasfilipe <lucasfilipe2323@icloud.com>
2022-04-15 16:06:05 -0300

    ciclo de vida dos componentes

--- primeirosite/src/App.js
+++ primeirosite/src/App.js
@@ -5,39 +5,32 @@ class App extends Component{
   constructor(props){
     super(props);
     this.state = {
-      nome: "Lucas",
-      contador: 0
+      hora: '00:00:00'
     };
-    this.aumetar = this.aumetar.bind(this);
-    this.diminuir = this.diminuir.bind(this);
+  
+  }
+  
+  componentDidMount(){
+      setInterval (() => {
+        this.setState({hora: new Date().toLocaleTimeString() })
+      }, 1000);
+  }
+  
+    componentDidUpdate(){
+      console.log('atualizou');
+  
   }
 
-  aumetar(){
-    let state = this.state;
-    state.contador += 1;
-    this.setState(state);
-  }
 
-  diminuir(){
-    let state = this.state;
-    state.contador -= 1;
-    this.setState(state)
-  }
     render(){
     return(
       
       <div>
-        <h1>Contatdor</h1>
-        <h3>
-          <button onClick={this.diminuir}>-</button>
-            {this.state.contador}
-          <button onClick={this.aumetar}>+</button>
-        </h3>
+        <h1>Meu Projeto {this.state.hora}</h1>
       </div>
       
     );
   }
 }
-
 
 export default App;

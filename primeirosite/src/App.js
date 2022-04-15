cadd22750afb2bcf82c6d7b35745b548d98a5b17
lucasfilipe <lucasfilipe2323@icloud.com>
2022-04-15 15:43:18 -0300

    aplicando state

--- primeirosite/src/App.js
+++ primeirosite/src/App.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
 
-class Equipe extends Component{
-  render(){
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      nome: "Lucas",
+      contador: 0
+    };
+    this.aumetar = this.aumetar.bind(this);
+    this.diminuir = this.diminuir.bind(this);
+  }
+
+  aumetar(){
+    let state = this.state;
+    state.contador += 1;
+    this.setState(state);
+  }
+
+  diminuir(){
+    let state = this.state;
+    state.contador -= 1;
+    this.setState(state)
+  }
+    render(){
     return(
       
-      //this referencia a este componente
       <div>
-        <h1>{this.props.nome}</h1>
-        <h2>{this.props.cargo}</h2>
-        <h3>{this.props.idade}</h3>
+        <h1>Contatdor</h1>
+        <h3>
+          <button onClick={this.diminuir}>-</button>
+            {this.state.contador}
+          <button onClick={this.aumetar}>+</button>
+        </h3>
       </div>
       
     );
   }
 }
 
-function App() {
-  return (
-    <div>
-      <Equipe nome="Lucas" cargo="Programador" idade="25"/>
-    </div>
-  );
-}
+
 export default App;
